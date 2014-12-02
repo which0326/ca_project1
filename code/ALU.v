45d0df60a44efcf7268f4d49fbd0674ca59dd74c
愷愷 <kevin830222@mobagel.com>
2014-12-02 20:29:31 +0800

    add additional files

---
+++ code/ALU.v
@@ -0,0 +1,49 @@
+module ALU(
+    data1_i,
+    data2_i,
+    ALUCtrl_i,
+    data_o,
+    Zero_o
+);
+
+// ports
+input       [31:0]    data1_i, data2_i;
+input       [2:0]     ALUCtrl_i;
+output reg  [31:0]  	 data_o, Zero_o;
+
+wire        [31:0]    add_o, sub_o, mul_o;
+wire        [31:0]    overflow;
+wire                  carry1_o, carry2_o;
+
+Adder32 add(data1_i, data2_i, 1'b0, add_o, carry1_o);
+Sub32   sub(data1_i, data2_i, 1'b0, sub_o, carry2_o);
+Mult32  mul(data1_i, data2_i, mul_o, overflow);
+
+always @(*) begin
+  case(ALUCtrl_i)
+    3'b010: begin   //  add
+      data_o  = add_o;
+      Zero_o  = carry1_o;
+    end
+    3'b110: begin   //  sub
+      data_o = sub_o;
+      Zero_o = carry2_o;
+    end
+    3'b000: begin   //  and
+      data_o = data1_i & data2_i;
+    end
+    3'b001: begin   //  or
+      data_o = data1_i | data2_i;
+    end
+    3'b011: begin   //  mul
+      data_o = mul_o;
+      Zero_o = overflow;
+    end
+    3'b111: begin   //  set-on-less-than
+      data_o = 32'hffffffff;
+    end
+  	endcase
+end
+
+
+endmodule
---
+++ code/ALU_Control.v
@@ -0,0 +1,31 @@
+module ALU_Control
+(
+    funct_i,
+    ALUOp_i,
+    ALUCtrl_o
+);
+
+// ports
+input       [5:0]   funct_i;
+input       [1:0]   ALUOp_i;
+output reg  [2:0]   ALUCtrl_o;
+
+always @(*) begin
+  case(ALUOp_i)
+    2'b11:  begin                                  // R-type
+              case(funct_i)
+                6'b100000:   ALUCtrl_o = 3'b010;   //  add
+                6'b100010:   ALUCtrl_o = 3'b110;   //  sub
+        	       6'b100100:   ALUCtrl_o = 3'b000;   //  and
+                6'b100101:   ALUCtrl_o = 3'b001;   //  or
+                6'b011000:   ALUCtrl_o = 3'b011;   //  mul
+                6'b101010:   ALUCtrl_o = 3'b111;   //  set-on-less-than
+              endcase
+            end
+    2'b00:  begin    	                             //  addi
+              ALUCtrl_o = 3'b010;                  //  add
+    end
+  endcase
+end
+
+endmodule
